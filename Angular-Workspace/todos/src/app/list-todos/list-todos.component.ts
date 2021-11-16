31fb9195bd1f4e5cfb2ad22300d392a469183ffd
Sean Sulham <seansulham@Seans-MBP.attlocal.net>
2021-11-15 19:21:17 -0500

    Began migrating To Do List business logic to Spring.  Completed To Do List itself, as well as adding getAll and Delete methods and connected to Angular.  Started the process for Update method.

--- Angular-Workspace/todos/src/app/list-todos/list-todos.component.ts
+++ Angular-Workspace/todos/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Todo } from '../A-Beans/todo';
+import { TodoDataService } from '../todo-data.service';
 
 @Component({
   selector: 'app-list-todos',
@@ -8,15 +10,47 @@ import { Todo } from '../A-Beans/todo';
 })
 export class ListTodosComponent implements OnInit {
 
-  todos = [
-    new Todo(1, "Learn Japanese", false, new Date()),
-    new Todo(2, "Learn Spanish", false, new Date()),
-    new Todo(3, "Learn Chinese", false, new Date())
-  ];
+  successMessage: string = "";
 
-  constructor() { }
+  todos!: Todo[];
+
+  // todos = [
+  //   new Todo(1, "Learn Japanese", false, new Date()),
+  //   new Todo(2, "Learn Spanish", false, new Date()),
+  //   new Todo(3, "Learn Chinese", false, new Date())
+  // ];
+
+  constructor(
+    private todoService: TodoDataService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.refreshTodos();
+  }
+
+  refreshTodos() {
+    this.todoService.retrieveAllTodos("zexl").subscribe(
+      response => {
+        this.todos = response;
+      }
+    )
+  }
+
+  update(id: number) {
+    console.log(`To Do Item ${id} Updated.`);
+    this.router.navigate(['updateTodo', id]);
+  }
+
+  delete(id: number) {
+    console.log(`To Do Item ${id} Deleted.`);
+    this.todoService.deleteTodo("zexl", id).subscribe(
+      response => {
+        console.log(response);
+        this.successMessage = `To Do List Item ${id} Deleted.`;
+        this.refreshTodos();
+      }
+    )
   }
 
 }
--- Angular-Workspace/todos/src/app/welcome/welcome.component.ts
+++ Angular-Workspace/todos/src/app/welcome/welcome.component.ts
@@ -9,7 +9,8 @@ import { WelcomeDataService } from '../welcome-data.service';
 })
 export class WelcomeComponent implements OnInit {
 
-  welcomeMessage: string = "";
+  welcomeMessage!: string;
+  errorMessage!: string;
   name = '';
 
   constructor(
@@ -23,12 +24,26 @@ export class WelcomeComponent implements OnInit {
   getWelcomeMessage() {
     console.log(this.service.executeHelloWorldBean());
     this.service.executeHelloWorldBean().subscribe(
-      response => console.log(response.message)
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
+    )
+  }
+
+  getPersonalizedWelcomeMessage() {
+    this.service.executeHelloWorldBeanWithPathVariable(this.name).subscribe(
+      response => this.handleSuccessfulResponse(response),
+      error => this.handleErrorResponse(error)
     )
   }
 
   handleSuccessfulResponse(response: any) {
-    console.log(response.message)
+    this.welcomeMessage = response.message;
+    this.errorMessage = "";
+  }
+
+  handleErrorResponse(error: any) {
+    this.errorMessage = error.error.message;
+    this.welcomeMessage = "";
   }
 
 }
