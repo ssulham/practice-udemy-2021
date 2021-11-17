7dceb5f6f3da6d5efc1e9b76d8e9853333fe6efc
Sean Sulham <seansulham@Seans-MBP.attlocal.net>
2021-11-17 17:50:25 -0500

    Added to Update Todo component.

--- STS-Workspace/todos/src/main/java/com/udemypractice/rest/webservices/controllers/TodoController.java
+++ STS-Workspace/todos/src/main/java/com/udemypractice/rest/webservices/controllers/TodoController.java
@@ -3,11 +3,15 @@ package com.udemypractice.rest.webservices.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemypractice.rest.webservices.beans.Todo;
@@ -25,9 +29,24 @@ public class TodoController {
 		return todoService.findAll();
 	}
 	
+	@GetMapping(path = "/users/{username}/todos/{id}")
+	public Todo getTodo(@PathVariable String username, @PathVariable long id) {
+		return todoService.findById(id);
+	}
+	
 	// POST /users/{username}/todos
+	@PostMapping(path = "/users/{username}/todos")
+	//More to come...
 	
 	// PUT /users/{username}/todos/{todo}
+	@PutMapping(path = "/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(
+			@PathVariable String username, 
+			@PathVariable long id,
+			@RequestBody Todo todo) {
+		Todo todoUpdated = todoService.save(todo);
+		return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+	}
 	
 	// DELETE /users/{username}/todos/{todo}
 	@DeleteMapping(path = "/users/{username}/todos/{id}")
